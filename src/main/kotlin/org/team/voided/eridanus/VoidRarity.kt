f346c24541b460f92e57336397ac1adfb5c85b6e
Mxz-Astatine <davebobsos910@gmail.com>
2023-05-21 12:36:25 +0100

    Added and modified rarities to be up to date

--- src/main/kotlin/org/team/voided/eridanus/VoidRarity.kt
+++ src/main/kotlin/org/team/voided/eridanus/VoidRarity.kt
@@ -4,10 +4,18 @@ import net.minecraft.util.Formatting
 
 enum class VoidRarity(formattingArg: Formatting) {
     COMMON(Formatting.WHITE),
-    RARE(Formatting.YELLOW),
-    CHAMPION(Formatting.GREEN),
-    LEGENDARY(Formatting.BLUE),
-    MYTHICAL(Formatting.LIGHT_PURPLE);
+    RARE(Formatting.GREEN),
+    LEGENDARY(Formatting.GOLD),
+    MYTHICAL(Formatting.DARK_PURPLE),
+    VOID(Formatting.BLACK),
+    LUNAR(Formatting.DARK_GRAY),
+    //There is no orange colour and I am not sure what to do about that so im going to set it as white
+    //until I know what to do.
+    MYSTICAL(Formatting.WHITE),
+    BOSS(Formatting.DARK_RED),
+    ELITE(Formatting.DARK_AQUA),
+    ASTRAL(Formatting.LIGHT_PURPLE),
+    LOST(Formatting.AQUA);
 
     private val formatting: Formatting = formattingArg
 
